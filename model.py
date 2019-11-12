2f077de873a28f49e24b527d3e22dd34d25341cf
dlike230 <dlike230@gmail.com>
2019-11-11 20:08:51 -0500

    Fixes, switched to MLP

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 from typing import List
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 import numpy as np
 
@@ -14,14 +16,14 @@ class Vectorizer:
         self.dimensionality_reducer = None
 
     def fit_transform(self, comments: List[str]):
-        self.base_model = TfidfVectorizer(max_features=10000)
+        self.base_model = TfidfVectorizer()
         X_train = self.base_model.fit_transform(comments)
-        self.dimensionality_reducer = PCA()
+        self.dimensionality_reducer = TruncatedSVD(n_components=300)
         X_train = self.dimensionality_reducer.fit_transform(X_train)
         return X_train
 
     def transform(self, comments: List[str]):
-        self.dimensionality_reducer.transform(self.base_model.transform(comments))
+        return self.dimensionality_reducer.transform(self.base_model.transform(comments))
 
 class Model:
 
@@ -32,7 +34,7 @@ class Model:
     def fit(self, comments_train: List[str], y_train):
         self.vectorizer = Vectorizer()
         X_train = self.vectorizer.fit_transform(comments_train)
-        self.model = SVC()
+        self.model = MLPClassifier()
         self.model.fit(X_train, y_train)
 
     def predict(self, comments_test: List[str]):
---
+++ test.py
@@ -0,0 +1,4 @@
+from model import Model
+
+model = Model()
+model.fit(["chicken is good", "chicken is great", "chicken is heavenly"], [0, 1, 2])
