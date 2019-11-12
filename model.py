bab8d05eaf62de49c1abfd62e481d621644cd6ab
dlike230 <dlike230@gmail.com>
2019-11-11 19:15:41 -0500

    Added typing

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
@@ -9,16 +11,16 @@ class Model:
         self.vectorizer = None
         self.model = None
 
-    def fit(self, comments_train, y_train):
+    def fit(self, comments_train: List[str], y_train):
         self.vectorizer = TfidfVectorizer()
         X_train = self.vectorizer.fit_transform(comments_train)
         self.model = SVC()
         self.model.fit(X_train, y_train)
 
-    def predict(self, comments_test):
+    def predict(self, comments_test: List[str]):
         X_test = self.vectorizer.transform(comments_test)
         return self.model.predict(X_test)
 
-    def score(self, comments_test, y_test):
+    def score(self, comments_test: List[str], y_test):
         predictions = self.predict(comments_test)
         return accuracy_score(predictions, y_test)
