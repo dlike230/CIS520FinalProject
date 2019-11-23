3fdf7c9b5b345dfaa524a0bfb1f6eca490411dfb
osimon8 <osimon8@students.d125.org>
2019-11-22 23:16:01 -0500

    added functionality to use other features

--- main.py
+++ main.py
@@ -1,31 +1,84 @@
 import numpy as np
-from model import Model
+from model import Model, Vectorizer
 # import sklearn.scikit
-# from sklearn.linear_model import LinearRegression, LogisticRegression
+# from sklearn.linear_model import, LinearRegression, LogisticRegression
 from numpy import linalg as LA
+from sklearn.decomposition import PCA, TruncatedSVD
 import matplotlib.pyplot as plt
 import random
 import os
+import csv
+import pandas as pd
+from sklearn import preprocessing
+import scipy.sparse
 
 # path = "C://User//Owen//Desktop//sarcasm test//train-balanced-sarcasm.csv"
 path = "train-balanced-sarcasm.csv"
-num_samples = 10000
+num_samples = 100000
 
-data = np.loadtxt(path, dtype='str', delimiter=',', skiprows=1, usecols=(0, 1), max_rows=num_samples)
+use_cols = (0, 1, 2, 3, 4, 9)
 
-y = data[:, 0].astype('int32')
-X = data[:, 1]
+df=pd.read_csv(path, sep=',',header=None, skiprows=1, usecols=use_cols, nrows=num_samples, quotechar = '"')
+
+data = df.values
+
+
+# cols = [1, 2]
+
+
+y = data[1:, 0].astype('int32')
+X = data[1:, 1:]
+
+
+class_columns = [1, 2] #username, subreddit
+text_columns = [0, 4]
+
+
+# le = preprocessing.LabelEncoder()
+
+# for i in class_columns:
+# 	X[: , i] = le.fit_transform(X[:, i])
+
+
+ohe = preprocessing.OneHotEncoder()
+cats = ohe.fit_transform(X[:, class_columns])
+
+svd = TruncatedSVD(n_components=1000)
+cats = svd.fit_transform(cats)
+
+text = X[:, text_columns]
+
+
+
+print(text)
+
+vect = Vectorizer(pca = True)
+
+newText = []
+
+for col in text.T:
+	newText.append(vect.fit_transform(col))
+
+text = np.hstack(newText)
 
 # np.random.seed(100)
 # p = np.random.permutation(len(X))
 # X, y = X[p], y[p]
+
+# X = scipy.sparse.hstack((text, cats))
+
+other = X[:, [i for i in range(X.shape[1]) if i not in text_columns and i not in class_columns]]
+
+X = np.concatenate((text, cats), axis = 1)
+
+print(X)
 
 X_train, y_train = X[:num_samples // 2], y[:num_samples // 2]
 X_test, y_test = X[num_samples // 2:], y[num_samples // 2:]
 
 model = Model()
 
-model.fit(X_train, y_train)
+model.fit(X_train, y_train, text_indices = [0, 4], categorical_indices = class_columns)
 
 print(model.score(X_test, y_test))
 
--- model.py
+++ model.py
@@ -6,39 +6,67 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+import scipy.sparse
 import numpy as np
 
 
 class Vectorizer:
 
-    def __init__(self):
+    def __init__(self, pca = False):
         self.base_model = None
         self.dimensionality_reducer = None
+        self.pca = pca
 
     def fit_transform(self, comments: List[str]):
-        self.base_model = TfidfVectorizer()
+        self.base_model = TfidfVectorizer(lowercase = False)
         X_train = self.base_model.fit_transform(comments)
-        self.dimensionality_reducer = TruncatedSVD(n_components=300)
-        X_train = self.dimensionality_reducer.fit_transform(X_train)
+        if self.pca:
+            self.dimensionality_reducer = TruncatedSVD(n_components=1000)
+            X_train = self.dimensionality_reducer.fit_transform(X_train)
         return X_train
 
     def transform(self, comments: List[str]):
-        return self.dimensionality_reducer.transform(self.base_model.transform(comments))
+        if self.pca:
+            return self.dimensionality_reducer.transform(self.base_model.transform(comments))
+        else:
+            return self.base_model.transform(comments)
 
 class Model:
 
     def __init__(self):
         self.vectorizer = None
         self.model = None
+        self.text_indices = None
 
-    def fit(self, comments_train: List[str], y_train):
-        self.vectorizer = Vectorizer()
-        X_train = self.vectorizer.fit_transform(comments_train)
-        self.model = MLPClassifier()
+    def fit(self, comments_train: List[str], y_train, text_indices : List[int] = None, categorical_indices = None):
+        self.vectorizer = Vectorizer(pca = True)
+                # X_train = np.append(X_train, x);
+        # if text_indices is not None:
+        #     self.text_indices = text_indices
+        # X_train = self.vectorize_columns(comments_train)
+        # print(X_train.shape)
+        X_train = comments_train
+        self.model = SVC()
         self.model.fit(X_train, y_train)
 
+
+    def vectorize_columns(self, comments_train):
+        n, p = comments_train.shape
+        if self.text_indices is None:
+            X_train = self.vectorizer.fit_transform(comments_train)
+        else:
+            X_train = comments_train[:, [i for i in range(p) if i not in self.text_indices]].astype('float64')
+            for col in comments_train[ :, self.text_indices].T:
+                x = self.vectorizer.fit_transform(col.astype('U'))
+                if X_train.shape[1] == 0:
+                    X_train = x
+                else:
+                    X_train = np.hstack((X_train, x))
+        return X_train
+
     def predict(self, comments_test: List[str]):
-        X_test = self.vectorizer.transform(comments_test)
+        # X_test = self.vectorize_columns(comments_test)
+        X_test = comments_test
         return self.model.predict(X_test)
 
     def score(self, comments_test: List[str], y_test):
