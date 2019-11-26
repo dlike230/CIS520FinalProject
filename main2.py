d648a726a508e7af43690685c9695d20e49b109a
dlike230 <dlike230@gmail.com>
2019-11-26 13:04:14 -0500

    Refactored code

--- main2.py
+++ main2.py
@@ -8,6 +8,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import ElasticNet, LogisticRegression
 from sklearn.svm import SVC
 
+from model import Model, Vectorizer, evaluate_model
+
 raw_df = pd.read_csv("Reviews.csv", sep=',', quotechar='"')
 df = raw_df.sample(n=10000)
 # df = raw_df
@@ -15,24 +17,11 @@ texts = df["Text"]
 texts = [Soup(text, features="html.parser").get_text() for text in texts]
 helpfulnessNumerators = df["HelpfulnessNumerator"]
 helpfulnessDenominators = df["HelpfulnessDenominator"]
-reviews = [score > 3 for score in df["Score"]]
 scores = [helpfulnessDenominator < 1 or helpfulnessNumerator / helpfulnessDenominator < 0.7769 for
 		  helpfulnessNumerator, helpfulnessDenominator in
 		  zip(helpfulnessNumerators, helpfulnessDenominators)]
-n_total = len(texts)
-n_train = int(n_total * 0.5)
 
-text_train = texts[:n_train]
-text_test = texts[n_train:]
-
-y_train = scores[:n_train]
-y_test = scores[n_train:]
-
-vectorizer = TfidfVectorizer(lowercase=True)
-dim_reducer = TruncatedSVD(n_components=1000)
-X_train = dim_reducer.fit_transform(vectorizer.fit_transform(text_train))
-X_test = dim_reducer.transform(vectorizer.transform(text_test))
-
-model = LogisticRegression(solver="lbfgs")
-model.fit(X_train, y_train)
-print(sklearn.metrics.fbeta_score(y_test, model.predict(X_test), 1))
+vectorizer = Vectorizer(pca=True, base_model=TfidfVectorizer(lowercase=True))
+model = Model(vectorizer=vectorizer, model=LogisticRegression(solver="lbfgs"))
+print(evaluate_model(model, texts, scores,
+			   score=lambda actual, predicted: sklearn.metrics.fbeta_score(actual, predicted, 1)))
