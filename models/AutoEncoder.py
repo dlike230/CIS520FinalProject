883a4117c8326b50333b1bb84b9ff71946d3e90c
dlike230 <dlike230@gmail.com>
2019-12-07 17:22:41 -0500

    Autoencoder

--- models/AutoEncoder.py
+++ models/AutoEncoder.py
@@ -1,45 +1,75 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-import tensorflow as tf
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+class AutoencoderNN(nn.Module):
+    def __init__(self, in_dim, n=64):
+        super(AutoencoderNN, self).__init__()
+        self.encoder = nn.Sequential(nn.Linear(in_dim, 256),
+                                     nn.ReLU(),
+                                     nn.Linear(256, n),
+                                     nn.ReLU(),)
+        self.decoder = nn.Sequential(nn.Linear(n, 256),
+                                     nn.ReLU(),
+                                     nn.Linear(256, in_dim),
+                                     nn.Tanh())
+
+    def forward(self, x):
+        model = nn.Sequential(self.encoder, self.decoder)
+        return model(x)
 
 
 class BagOfWordsAutoEncoder:
 
-    def __init__(self):
+    def __init__(self, num_epochs=3):
         self.vectorizer = TfidfVectorizer()
         self.encoder = None
         self.decoder = None
         self.model = None
+        self.num_epochs = num_epochs
 
     def fit_transform(self, reviews):
         vectors = self.vectorizer.fit_transform(reviews).toarray()
-
-        print(vectors.shape)
-        self.encoder = tf.keras.Sequential()
-        self.encoder.add(tf.keras.layers.InputLayer(input_shape=(vectors.shape[1],)))
-        self.encoder.add(tf.keras.layers.Dense(100, activation="relu", use_bias=False))
-        self.encoder.add(tf.keras.layers.Dense(50, activation="relu", use_bias=False))
-        self.encoder.add(tf.keras.layers.Dense(2, use_bias=False))
-
-        self.decoder = tf.keras.Sequential()
-        self.encoder.add(tf.keras.layers.InputLayer(input_shape=(2,)))
-        self.decoder.add(tf.keras.layers.Dense(50, input_shape=(2,), use_bias=False, activation="relu"))
-        self.decoder.add(tf.keras.layers.Dense(100, use_bias=False, activation="relu"))
-        self.decoder.add(tf.keras.layers.Dense(vectors.shape[1], use_bias=False))
-
-        self.model = tf.keras.Sequential([self.encoder, self.decoder])
-        self.model.compile(loss="mean_squared_error", optimizer="sgd")
-        self.model.fit(x=vectors, y=vectors, epochs=3)
+        in_size = vectors.shape[1]
+        self.model = AutoencoderNN(in_size, n=2)
+        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1E-4)
+        trainloader = DataLoader(vectors, batch_size=32, shuffle=True)
+        batch_losses = []
+        for epoch in range(self.num_epochs):
+            total_loss = 0
+            num_items = 0
+            print("Epoch %d" % epoch)
+            for i, data in enumerate(trainloader):
+                data = data.float()
+                optimizer.zero_grad()
+                output = self.model.forward(data)
+                loss = criterion(output, data)
+                loss_val = loss.item()
+                batch_losses.append(loss_val)
+                total_loss += loss_val
+                num_items += 1
+                loss.backward()
+                optimizer.step()
+                if i % 20 == 0:
+                    print("Running loss estimate: %f" % (total_loss / num_items))
+        plt.plot(batch_losses)
+        plt.show()
         return self._transform(vectors)
 
     def _transform(self, vectors):
-        return self.encoder.predict(vectors)
+        return self.model.encoder(torch.from_numpy(vectors).float()).detach().numpy()
 
     def reconstruction_error(self, reviews):
         vectors = self.vectorizer.transform(reviews).toarray()
         encoded = self._transform(vectors)
-        decoded = self.decoder.predict(encoded)
-        return np.linalg.norm(decoded, vectors) / len(vectors)
+        decoded = np.array(self.decoder.predict(encoded))
+        print(decoded.shape)
+        print(vectors.shape)
+        return sum(np.linalg.norm(decoded_vec, vector) for vector, decoded_vec in zip(vectors, decoded)) / len(vectors)
 
     def transform(self, reviews):
         return self._transform(self.vectorizer.transform(reviews).toarray())
