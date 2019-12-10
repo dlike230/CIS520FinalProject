822c253fbb6397c7bed097ee438b1e0669b8075a
osimon8 <osimon8@students.d125.org>
2019-12-10 13:44:50 -0500

    added ensemble pipeline

---
+++ pipeline/EnsemblePipeline.py
@@ -0,0 +1,17 @@
+from models.dumb import Dumb
+from pipeline.Pipeline import Pipeline
+
+
+class EnsemblePipeline(Pipeline):
+
+    def __init__(self):
+        super().__init__("Score", 0.5)
+
+    def make_model(self):
+        return Dumb()
+
+    def label_func(self, item):
+        return 1 if item > 3 else 0
+
+
+EnsemblePipeline().evaluate()
