5008ea9ea5c0578b9d8055afa3ce8ad032088e86
Owen Simon <osimon922@gmail.com>
2019-12-10 01:02:40 -0500

    added boosting methods, fixed dumb

--- models/dumb.py
+++ models/dumb.py
@@ -3,9 +3,12 @@ class Dumb:
         self.model = None
 
     def fit(self, train_strings, y_train):
-        c1 = sum(i for i in y_train if i == 0)
+        c1 = 0
+        for y in y_train:
+            if y == 0:
+                c1 += 1
         c2 = len(y_train) - c1
-        self.model = max(c1, c2)
+        self.model = 0 if c1 > c2 else 1
 
     def predict(self, reviews_test):
         return [self.model] * len(reviews_test)
--- pipeline/RatingPredictorGradientBoostingDimReduction.py
+++ pipeline/RatingPredictorGradientBoostingDimReduction.py
@@ -1,21 +1,22 @@
 import sklearn
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 
 from models.WordAutoEncoder import BagOfWordsAutoEncoder
 from models.model import Model, Vectorizer
 from pipeline.Pipeline import Pipeline
 
 
-class RatingPredictorLogisticDimReduction(Pipeline):
+class RatingPredictorGradientBoostingDimReduction(Pipeline):
 
     def __init__(self):
         super().__init__("Score", 0.5)
 
     def make_model(self):
-        return Model(model=LogisticRegression(), vectorizer=Vectorizer(pca=True))
+        return Model(model=GradientBoostingClassifier(), vectorizer=Vectorizer(pca=True))
 
     def label_func(self, item):
         return 1 if item > 3 else 0
 
 
-RatingPredictorLogisticDimReduction().evaluate()
+RatingPredictorGradientBoostingDimReduction().evaluate()
