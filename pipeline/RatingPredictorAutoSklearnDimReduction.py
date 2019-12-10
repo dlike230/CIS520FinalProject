6a9747cedc7b9a6912b0b779808f52f780ebb9db
osimon8 <osimon8@students.d125.org>
2019-12-10 13:27:29 -0500

    added auto sklearn pipeline

---
+++ pipeline/RatingPredictorAutoSklearnDimReduction.py
@@ -0,0 +1,24 @@
+from autosklearn.classification import AutoSklearnClassifier
+
+from models.model import Model, Vectorizer
+from pipeline.Pipeline import Pipeline
+
+
+class RatingPredictorAutoSklearnDimReduction(Pipeline):
+
+    def __init__(self):
+        super().__init__("Score", 0.5)
+
+    def make_model(self):
+        return Model(model=AutoSklearnClassifier(), vectorizer=Vectorizer(pca=True))
+
+    def label_func(self, item):
+        return 1 if item > 3 else 0
+
+    def evaluate(self):
+        super().evaluate()
+        self.model.model.sprint_statistics()
+        self.model.model.show_models()
+
+
+RatingPredictorAutoSklearnDimReduction().evaluate()
